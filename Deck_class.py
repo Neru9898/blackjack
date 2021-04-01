de7e62dae7ffb97f97890d9e8211850e391bbdf6
Nerushan Naguleswaran <neonerushan@gmail.com>
2021-04-01 04:01:38 -0400

    Deck class update V1.0

--- Deck_class.py
+++ Deck_class.py
@@ -10,14 +10,29 @@ class Card:
             'Diamonds':['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
         }
 
+    def  card_values(self):
+        self.card_value = {
+            'Ace': 1,
+            '2': 2,
+            '3': 3,
+            '4': 4,
+            '5': 5,
+            '6': 6,
+            '7': 7,
+            '8': 8,
+            '9': 9,
+            '10': 10,
+            'J': 10,
+            'Q': 10,
+            'K':10
+        }
+
+
 
 class Deck:
     def __init__(self):
         self.deck = Card().base
     
-    def shuffle_deck(self):
-        pass
-
     def draw_card(self):
         self.suits_drawn = random.choice(list(self.deck.keys()))
         self.num_drawn = random.choice(self.deck[self.suits_drawn])
@@ -31,10 +46,6 @@ class Deck:
     def reset_deck(self):
         self.deck = Card().base
 
-t = Deck()
-
-
-print(t.draw_card())
-print(t.pop_deck())
-print(t.deck,'final')
-
+    def deck_empty_check(self):
+        if(not (self.deck['Hearts'] and self.deck['Spades'] and self.deck['Clubs'] and self.deck['Diamonds'])):
+            print('Deck is empty and ned to reset')
