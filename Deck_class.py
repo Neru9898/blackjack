eeec892736b84c0454241a80568254d0ff9dc665
Nerushan Naguleswaran <neonerushan@gmail.com>
2021-04-01 21:00:59 -0400

    update

--- Deck_class.py
+++ Deck_class.py
@@ -31,24 +31,21 @@ class Card:
 class Deck:
     def __init__(self):
         self.deck = Card().base
+        self.deck_status_empty  = 0
     
     def draw_card(self):
         self.suits_drawn = random.choice(list(self.deck.keys()))
         for suits in self.deck:
-            deck_status = self.deck_empty_check()
-            if deck_status != True:
+            self.deck_empty_check()
+            if self.deck_status_empty  != True:
                 if self.deck[self.suits_drawn] != []:
                     self.num_drawn = random.choice(self.deck[self.suits_drawn])
                     self.index_num = self.deck[self.suits_drawn].index(self.num_drawn)
+                    return self.suits_drawn, self.num_drawn, self.index_num
                 else:
                     self.draw_card()
             else:
-                print('bye')
                 break
-
-        
-
-        return self.suits_drawn, self.num_drawn, self.index_num
 
     def pop_deck(self):
         self.deck[self.suits_drawn].pop(self.index_num)
@@ -59,9 +56,13 @@ class Deck:
 
     def deck_empty_check(self):
         if((self.deck['Hearts'] == [] and self.deck['Spades'] == [] and self.deck['Clubs'] == [] and self.deck['Diamonds'] == [])):
-            print('Deck is empty and ned to reset')
-            self.empty = True
-            return self.empty
+            self.deck_status_empty = True
+        else:
+            self.deck_status_empty = False
+        
+
+        
+    
             
 
 
@@ -78,22 +79,32 @@ class BlackJack:
     
     def current_hand(self):
         current = self.deck_class.draw_card()
-        deck_status = self.deck_class.deck_empty_check()
-        if (deck_status != True):
+        # deck_status = self.deck_class.deck_empty_check()
+        if (self.deck_class.deck_status_empty  != True):
             self.deck_class.pop_deck()
+            print(current)
             return current
         else:
-            return None
-        print(self.game_deck)
+            self.game_reset()
+            
+
         
     def value_of_hand(self,hand):
         current_suits, current_num_drawn, current_index_num  = self.current_hand()
 
+    def game_reset(self):
+        reset = input("Deck is empty, Do you want to cont or no:")
+        if reset == 'y':
+            self.deck_class.reset_deck()
+            self.current_hand()
+        else:
+            print('bye',self.game_deck)
+
             
 
 
-t = BlackJack()
+f = BlackJack()
 i = 0
-while i < 53:
+while i < 56:
     i += 1
-    print(t.current_hand())
+    f.current_hand()
